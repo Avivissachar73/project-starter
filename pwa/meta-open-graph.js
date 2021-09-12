e79b5ca475dbac10fffc366da1060c12c2af924d
Aviv Issachar <avivi@misterbit.co.il>
2021-09-12 22:38:53 +0300

    added og image

--- pwa/meta-open-graph.js
+++ pwa/meta-open-graph.js
@@ -1,7 +1,7 @@
 const config =  {
   "title": "My App",
   "description": "Created by Aviv Issachar",
-  "image": "",
+  "image": "https://s.abcnews.com/images/Lifestyle/ht_pudding_the_fox_03_mt_140821_4x3t_608.jpg",
   "url": ""
 }
 
